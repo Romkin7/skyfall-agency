d5a6e990961b3160fd99abb0f7c6e5f6ff5d203b
Roman Tuomisto <romantuomisto@gmail.com>
2021-09-13 12:40:15 +0300

    fix imports 2

--- frontend/src/pages/referenssit/{StrapiReference.slug}.js
+++ frontend/src/pages/referenssit/{StrapiReference.slug}.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 import Hero from "../../components/Hero";
 import Markdown from "react-markdown";
 
